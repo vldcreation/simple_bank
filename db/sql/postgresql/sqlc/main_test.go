c528d5873c33646356dd00cde9c9f4387d77fa63
Vicktor Desrony <vldcreation21@gmail.com>
2024-05-22 02:37:27 +0700

    update db env

--- db/sql/postgresql/sqlc/main_test.go
+++ db/sql/postgresql/sqlc/main_test.go
@@ -11,14 +11,22 @@ import (
 
 const (
 	dbdriver = "postgres"
-	dbSource = "postgresql://postgres:secret@localhost:5432/golang_masterclass?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	var (
+		err               error
+		POSTGRES_USER     = os.Getenv("POSTGRES_USER")
+		POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+		POSTGRES_DB       = os.Getenv("POSTGRES_DB")
+		POSTGRES_HOST     = os.Getenv("POSTGRES_HOST")
+		POSTGRES_PORT     = os.Getenv("POSTGRES_PORT")
+	)
+
+	dbSource := "postgresql://" + POSTGRES_USER + ":" + POSTGRES_PASSWORD + "@" + POSTGRES_HOST + ":" + POSTGRES_PORT + "/" + POSTGRES_DB + "?sslmode=disable"
 
 	testDB, err = sql.Open(dbdriver, dbSource)
 	if err != nil {
